7461dd06fc2d64ae8847af1ef1fe14d633fefdeb
Nian0bussou <nianlegare@outlook.com>
2024-12-10 11:36:31 -0500

    jsldfk

--- fzf-custom.sh
+++ fzf-custom.sh
@@ -7,9 +7,9 @@ start_path="."
 # Parse the arguments
 for arg in "$@"; do
     case "$arg" in
-        -d) mode="directories" ;;
-        -f) mode="files" ;;
-        *) start_path="$arg" ;; # Assume the argument is the directory path
+    -d) mode="directories" ;;
+    -f) mode="files" ;;
+    *) start_path="$arg" ;; # Assume the argument is the directory path
     esac
 done
 
@@ -21,21 +21,22 @@ fi
 
 while true; do
     # Select mode for fzf
+    gggmode="f"
     if [ "$mode" = "directories" ]; then
-        selection=$(find "$start_path" \( -path '*/.git' -o -path '*/.obsidian' \) -prune -o -type d -print | fzf --preview "batcat --color=always --style=numbers --line-range=:500 {}")
-    elif [ "$mode" = "files" ]; then
-        selection=$(find "$start_path" \( -path '*/.git' -o -path '*/.obsidian' \) -prune -o -type f -print | fzf --preview "batcat --color=always --style=numbers --line-range=:500 {}")
+        gggmode="d"
     fi
+
+    selection=$(find "$start_path" \
+        \( -path '*/.git' -o -path '*/.obsidian' -o -path '*/.venv' -o -path '*/.idea' \) -prune -o \
+        -type $gggmode -print | fzf --preview "batcat --color=always --style=numbers --line-range=:500 {}")
 
     # Trim whitespace from the selection
     selection=$(echo "$selection" | xargs)
 
     # Check if selection is empty
     if [ -z "$selection" ]; then
-        echo "Exiting fzf."
         break
     fi
-
     # Open the selection in vim
-    $HOME/.local/bin/lvim "$selection"
+    /usr/local/bin/nvim "$selection"
 done
